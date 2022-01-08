effef9b94f99f606b8719d766ef1935e97279817
chanjeff <jeffchan142@gmail.com>
2022-01-08 20:04:32 +0800

    feat: wall game

---
+++ commands/wallGame.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Settings = require("../models/setting");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wall-game')
+		.setDescription('wall game against someone')
+		.addUserOption((option) =>
+			option
+			.setName("user")
+			.setDescription("who you want to fu*k")
+			.setRequired(true)
+		),
+	async execute(interaction) {
+		const target = interaction.options.get("user").value;
+		await interaction.reply(`I am playing wall game against ${target}.`);
+	},
+};
