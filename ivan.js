22bd1013b8f55ac5d10ef50a63d5746e1be924aa
chanjeff <jeffchan142@gmail.com>
2022-01-08 16:36:59 +0800

    migrate from python to nodejs

---
+++ ivan.js
@@ -0,0 +1,22 @@
+const { Client, Intents } = require("discord.js");
+
+// create client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+// on bot ready/started
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.on("message", (message) => {
+    // prevent self loop
+    if (message.author == client.user) {
+        return;
+    }
+    // replace "7" with "ivan"
+    if (message.content.contains("7")) {
+        message.reply(message.content.replace("7", " **Ivan** "));
+    }
+})
+
+client.login(process.env.token);
