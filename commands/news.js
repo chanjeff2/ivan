17ffdadef7668b1df6dabbe2aee3e41401d5a62d
CurlyRhapsody <matthewchan60792@gmail.com>
2022-01-16 19:18:07 +0800

    New feature: news headline generator

---
+++ commands/news.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageAttachment } = require('discord.js');
+const { createCanvas, loadImage } = require('canvas');
+const newsPicURL = __dirname + '/../public/images/news.png'
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('news')
+		.setDescription('Publish a news on The Ivan7 News')
+		.addStringOption((option) =>
+			option
+			.setName("title")
+			.setDescription("the title of the news")
+			.setRequired(true)
+		),
+    async execute(interaction) {
+        const title = interaction.options.getString("title").toUpperCase();
+
+        const newspaper = await loadImage(newsPicURL);
+
+        const canvas = createCanvas(newspaper.naturalWidth, newspaper.naturalHeight);
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(newspaper, 0, 0);
+        let fontSize = 96;
+
+        do {
+            ctx.font = `bold ${fontSize -= 4}px arial`;
+        } while (ctx.measureText(title).width > canvas.width - 70);
+        ctx.fillStyle = '#535353';
+        const centerLoc = (canvas.width/2 - ctx.measureText(title).width/2)
+        ctx.fillText(title, centerLoc, 290);
+
+        const img = canvas.toBuffer();
+        const file = new MessageAttachment(img, "file.png");
+        await interaction.reply({ files: [file] });
+    },
+};
